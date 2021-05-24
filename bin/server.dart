48c34629c83474b8e8aaa86f5c9a6f2befd44eb6
Naga Chiang <nagachiangdev@gmail.com>
2021-05-24 18:01:46 +0800

    Test

---
+++ bin/server.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+Future main(List<String> arguments) async {
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final router = Router()..get('/iap', _iapHandler);
+  final cascade = Cascade().add(router);
+  final pipeline =
+      Pipeline().addMiddleware(logRequests()).addHandler(cascade.handler);
+  final server = await serve(
+    pipeline,
+    InternetAddress.anyIPv4,
+    port,
+  );
+}
+
+Future<Response> _iapHandler(Request request) async {
+  final body = await request.readAsString();
+  print(body);
+
+  return Response.ok(body);
+}
