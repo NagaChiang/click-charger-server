44cae54aeaea4da7f87618d2c2120a1efa33a748
Naga Chiang <nagachiangdev@gmail.com>
2021-06-06 14:48:07 +0800

    Add version logging on start

--- bin/server.dart
+++ bin/server.dart
@@ -1,8 +1,15 @@
 import 'dart:io';
 
 import 'package:click_charger_server/click_charger_server.dart';
+import 'package:pubspec_parse/pubspec_parse.dart';
 
 Future main(List<String> arguments) async {
+  final pubspecFile = File('pubspec.yaml');
+  final pubspec = Pubspec.parse(await pubspecFile.readAsString());
+  final version = pubspec.version.toString();
+
+  print('Click Charger Server v$version');
+
   final internetAddress = InternetAddress.anyIPv4;
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
 
