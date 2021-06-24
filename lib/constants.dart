08db099ccc38f2ecc5e0e7c97b75986abb099eb6
Naga Chiang <nagachiangdev@gmail.com>
2021-06-24 23:28:41 +0800

    Change rewarded ad cooldown to 12 hours

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,4 +4,4 @@ const verifyApiName = 'verify';
 const useBoostApiName = 'useBoost';
 const rewardedAdApiName = 'rewardedAd';
 const Duration durationPerBoost = Duration(hours: 4);
-const Duration rewardedAdCooldown = Duration(hours: 20);
+const Duration rewardedAdCooldown = Duration(hours: 12);
